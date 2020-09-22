863fb85fec37d79f4b08c337571bf2fc6e2bfc69
Erik Olofsson <erik.olofsson@favro.com>
2020-09-22 20:07:37 +0200

    Up meteor version to get jquery 3.0
    Set name and version

--- package.js
+++ package.js
@@ -1,10 +1,15 @@
 Package.describe({
+name: "mrt:jquery-easing",
+version: "1.3.0",
 summary : "GSGD's jQuery easing plugin for Meteor"
 });
 
 Package.on_use(function(api) {
+api.versionsFrom('METEOR@1.10.2');
+
 api.use('jquery', 'client');
 api.add_files([
 	'lib/js/jquery.easing.js'
 ], 'client');
 });
+
